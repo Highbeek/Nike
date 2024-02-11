13c2e4e94ecb43d0ad1dd7a6f3e3d0aac0eeb9c9
Ibukun <iagboola@kelmaridholdings.com>
2024-02-11 08:10:49 +0100

    nike landing page

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import {
+  Hero,
+  Services,
+  SpecialOffers,
+  SuperQuality,
+  Footer,
+  CustomerReviews,
+} from "./sections";
+import PopularProducts from "./sections/PopularProducts";
+import Subscribe from "./sections/Subscribe";
+import Nav from "./component/Nav";
+
+export default function App() {
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x padding-y-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+}
---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+export { Hero, Services, SpecialOffers, SuperQuality, Footer, CustomerReviews };
